78d9f346351ae7f394befc6c9c19d0c76fd5b1a2
MichaelTe <m.tedeev@student.vu.nl>
2024-06-10 19:17:26 +0200

    added flags push function in ArrayStack

--- src/main/java/com/williamfiset/algorithms/datastructures/stack/ArrayStack.java
+++ src/main/java/com/williamfiset/algorithms/datastructures/stack/ArrayStack.java
@@ -2,6 +2,7 @@ package com.williamfiset.algorithms.datastructures.stack;
 
 import java.util.Arrays;
 import java.util.EmptyStackException;
+import java.util.*;
 
 /**
  * @author liujingkun
@@ -28,14 +29,22 @@ public class ArrayStack<T> implements Stack<T> {
 
   @Override
   public void push(T elem) {
+    CoverageTracker.setBranchReached(1);
     if (size == capacity) {
       increaseCapacity();
     }
+    // Instrumentation: Set a flag before the first statement
+    CoverageTracker.setBranchReached(3);
     data[size++] = elem;
+
+    // Write coverage information to console at the end of the method
+    CoverageTracker.writeCoverageToConsole();
   }
 
   // Increase the capacity to store more elements.
   private void increaseCapacity() {
+    // Branch ID: 2
+    CoverageTracker.setBranchReached(2);
     capacity *= 2;
     data = Arrays.copyOf(data, capacity);
   }
@@ -54,5 +63,35 @@ public class ArrayStack<T> implements Stack<T> {
   public T peek() {
     if (isEmpty()) throw new EmptyStackException();
     return (T) data[size - 1];
+  }
+
+  public class CoverageTracker {
+    private static Map<Integer, Boolean> branchCoverage = new HashMap<>();
+
+    // Initialize branch coverage map with branch IDs and set all flags to false
+    static {
+      branchCoverage.put(1, false); // Branch ID for entering method
+      branchCoverage.put(2, false); // Branch ID for increaseCapacity() method
+      branchCoverage.put(3, false); // Branch ID for data[size++]
+    }
+
+    // Method to set a flag for a branch when it's reached
+    public static void setBranchReached(int branchID) {
+      branchCoverage.put(branchID, true);
+    }
+
+    // Method to get coverage information
+    public static Map<Integer, Boolean> getBranchCoverage() {
+      return branchCoverage;
+    }
+
+    // Method to write branch coverage information to console
+    public static void writeCoverageToConsole() {
+      System.out.println("Branch Coverage Information:");
+      for (Map.Entry<Integer, Boolean> entry : branchCoverage.entrySet()) {
+        System.out.println("Branch ID: " + entry.getKey() + ", Covered: " + entry.getValue());
+      }
+    }
+
   }
 }
