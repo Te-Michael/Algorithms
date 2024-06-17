fd812733b36292b88692b9bd84639c0a8a2b9316
Oliwer Dembicki <oliwer132@gmail.com>
2024-06-17 17:28:38 +0200

    updated CoverageTracker

--- src/main/java/com/williamfiset/algorithms/CoverageTracker.java
+++ src/main/java/com/williamfiset/algorithms/CoverageTracker.java
@@ -11,22 +11,12 @@ public class CoverageTracker {
         branchCoverage.put(branchID, true);
     }
 
-    public static void clearBranches() {
-        branchCoverage = new HashMap<>();
-    }
-
     public static void setTotalBranches(int branches) {
         for(int i = 0; i < branches; i++) {
             branchCoverage.put(i, false);
         }
     }
-
-    // Method to get coverage information
-    public static Map<Integer, Boolean> getBranchCoverage() {
-        return branchCoverage;
-    }
-
-    // Method to write branch coverage information to console
+    
     public static void writeCoverageToConsole() {
         System.out.println("Branch Coverage Information:");
         for (Map.Entry<Integer, Boolean> entry : branchCoverage.entrySet()) {
