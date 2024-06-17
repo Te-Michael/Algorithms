6702d39ca85f0dc316f78273fbb049fcdc22cb22
Oliwer Dembicki <oliwer132@gmail.com>
2024-06-17 17:34:02 +0200

    little update CoverageTracker

--- src/main/java/com/williamfiset/algorithms/CoverageTracker.java
+++ src/main/java/com/williamfiset/algorithms/CoverageTracker.java
@@ -5,8 +5,6 @@ import java.util.Map;
 
 public class CoverageTracker {
     private static Map<Integer, Boolean> branchCoverage = new HashMap<>();
-
-    // Method to set a flag for a branch when it's reached
     public static void setBranchReached(int branchID) {
         branchCoverage.put(branchID, true);
     }
@@ -15,8 +13,6 @@ public class CoverageTracker {
             branchCoverage.put(i, false);
         }
     }
-    
-    // Method to write branch coverage information to console
     public static void writeCoverageToConsole() {
         System.out.println("Branch Coverage Information:");
         for (Map.Entry<Integer, Boolean> entry : branchCoverage.entrySet()) {
