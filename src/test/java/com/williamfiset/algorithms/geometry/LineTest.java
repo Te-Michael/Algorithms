905c182614a79fab253e9dd6246f67b901a3361b
Oliwer Dembicki <oliwer132@gmail.com>
2024-06-22 23:14:04 +0200

    Added Hugo's test to Line

--- src/test/java/com/williamfiset/algorithms/geometry/LineTest.java
+++ src/test/java/com/williamfiset/algorithms/geometry/LineTest.java
@@ -95,4 +95,15 @@ public class LineTest {
         Line l2 = new Line(0.0,0.0,3.0,2.0);
         assertEquals(intersection(l1, l2), new Point2D.Double(0.0, 0.0));
     }
+
+    @Test
+    void testGetPerpBisector() {
+        double x1 = 0, y1 = 0, x2 = 4, y2 = 0;
+        Line bisector1 = Line.getPerpBisector(x1, y1, x2, y2);
+        assertEquals("1.0x + 0.0y = 2.0", bisector1.toString());
+
+        x1 = 2; y1 = 1; x2 = 2; y2 = 5;
+        Line bisector2 = Line.getPerpBisector(x1, y1, x2, y2);
+        assertEquals("0.0x + 1.0y = 3.0", bisector2.toString());
+    }
 }
